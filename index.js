c5d4732930ab7dc68c156560ac977b8d8ae72312
ashikalahi <ashikalahi@gmail.com>
2024-02-08 16:14:12 +0600

    add new files

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+function calculateMoney(ticketSale) {
+     if(isNaN(ticketSale) || ticketSale < 0){
+     return 'Invalid Number'}
+     const ticketPrice = 120;
+     const totalTicket = ticketPrice * ticketSale;
+     const darYan  = 500;
+     const lunchPerPerson = 50;
+     const issTaff = 8;
+     const fixKroroc = issTaff * lunchPerPerson + darYan ;
+     const totalAmount = totalTicket - fixKroroc;
+     return totalAmount;
+      }
+
+
+
+      
+function checkName(name) {
+ if(typeof name !== 'string'){
+      return 'invalid';
+}
+const lowerCase = name.toLowerCase();
+const allNames = ['tonoy', 'utsho' , 'roide', 'shanto'];
+const nameWords = ['A', 'y', 'i' , 'e' , 'o' , 'u', 'w'];
+const result = lowerCase.charAt(lowerCase.length -1);
+for(let names of allNames){
+      if(lowerCase.includes(names)){
+            return 'Bad Name';
+      }
+}
+    if(nameWords.includes(result)){
+      return 'Good Name'
+}
+   else{
+      return'Bad Name';
+}
+      }
+
+
+
+
 function deleteInvalids(array){
         if(!Array.isArray(array)){
             return 'Invaid Array';
